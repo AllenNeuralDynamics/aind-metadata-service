23eb66eaa9dba6d0ecc7900bbb33da59e7f82661
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2022-10-20 12:44:02 -0700

    Feat 2 mouse metadata (#6)
    * feat: adds ci workflows
    
    * feat: adds pyodbc dependency
    
    * feat: updates readme
    
    * feat: adds rudimentary poc
    
    * feat: updates typo in Dockerfile
    
    * feat: updates readme and dockerfile
    
    * feat: adds unit tests and docstrings
    
    * feat: adds type to functions signatures. Closes cursor.
    
    * feat: adds AIND as author, runs linter

---
+++ tests/test_client.py
@@ -0,0 +1,121 @@
+"""Module to test LabTracks Client methods"""
+
+import decimal
+import unittest
+from unittest.mock import Mock, patch
+
+import pyodbc
+
+from aind_metadata_service.client import ErrorResponses, LabTracksClient
+
+
+class TestLabTracksClient(unittest.TestCase):
+    """Tests client methods"""
+
+    driver = "{FreeTDS}"
+    server = "0.0.0.0"
+    port = "1234"
+    db = "LabTracksDB"
+    user = "LabTracksUser"
+    password = "LabTracksPassword"
+    lb_client = LabTracksClient(
+        driver=driver,
+        server=server,
+        port=port,
+        db=db,
+        user=user,
+        password=password,
+    )
+
+    def test_labtracks_client_connection_string(self) -> None:
+        """Tests client connection string is created correctly"""
+        expected_connection_string = (
+            f"Driver={self.driver};"
+            f"Server={self.server};"
+            f"Port={self.port};"
+            f"Database={self.db};"
+            f"UID={self.user};"
+            f"PWD={self.password};"
+        )
+
+        self.assertEqual(
+            self.lb_client.connection_str, expected_connection_string
+        )
+
+    @patch("pyodbc.connect")
+    def test_labtracks_client_create_session(self, mock_connect: Mock) -> None:
+        """
+        Tests client session method with mocked responses
+        Parameters
+        ----------
+        mock_connect : Mock
+            A mocked Connection class that can be used to override methods
+            without connecting to sqlserver
+
+        Returns
+        -------
+            pass
+
+        """
+
+        def mock_fetchall(_):
+            """Mock the fetchall method in
+            pyodbc.connect().cursor().fetchall()"""
+            return [
+                ("A", decimal.Decimal(403231)),
+                ("A", decimal.Decimal(499294)),
+            ]
+
+        type(mock_connect().cursor()).description = (
+            ("Animal_Status", str.__class__, None, 1, 1, 0, False),
+            ("ID", decimal.Decimal.__class__, None, 14, 14, 0, False),
+        )
+        type(mock_connect().cursor()).fetchall = mock_fetchall
+
+        session = self.lb_client.create_session()
+        example_query = "SELECT TOP 2 ANIMAL_STATUS, ID FROM ANIMALS_COMMON;"
+        response = self.lb_client.submit_query(session, example_query)
+        handled_response = self.lb_client.handle_response(response)
+        expected_handled_response = [
+            {"animal_status": "A", "id": decimal.Decimal("403231")},
+            {"animal_status": "A", "id": decimal.Decimal("499294")},
+        ]
+        mock_connect.assert_called()
+        self.assertEqual(expected_handled_response, handled_response)
+        self.lb_client.close_session(session)
+
+    @patch("pyodbc.connect")
+    def test_labtracks_client_submit_query_error(
+        self, mock_connect: Mock
+    ) -> None:
+        """
+        Tests that pyodbc errors are returned to client properly
+        Parameters
+        ----------
+        mock_connect : Mock
+            A mocked Connection class that can be used to override methods
+            without connecting to sqlserver
+
+        Returns
+        -------
+            pass
+        """
+
+        def mock_cursor(_):
+            """Mock the pyodbc.cursor() function."""
+            raise pyodbc.ProgrammingError
+
+        type(mock_connect.return_value).cursor = mock_cursor
+
+        session = self.lb_client.create_session()
+        response1 = self.lb_client.submit_query(session, "SOMETHING BAD")
+        expected_response = {
+            "msg": f"{ErrorResponses.pyodbc_error.value}: " f"ProgrammingError"
+        }
+        mock_connect.assert_called()
+        self.assertEqual(response1, expected_response)
+        self.lb_client.close_session(session)
+
+
+if __name__ == "__main__":
+    unittest.main()
