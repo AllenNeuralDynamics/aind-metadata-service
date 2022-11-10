24a1a7fc85b92083a32e0f9ebbf6b76053f7c480
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2022-11-10 10:00:59 -0800

    feat: sql string in test

--- tests/test_query_builder.py
+++ tests/test_query_builder.py
@@ -36,7 +36,8 @@ class TestLabTracksQueryBuilder(unittest.TestCase):
             "    ON AC.PATERNAL_INDEX = PATERNAL.ID"
             "    LEFT OUTER JOIN SPECIES S "
             "    ON AC.SPECIES_ID = S.ID"
-            "    LEFT OUTER JOIN GROUPS ON MATERNAL.GROUP_ID=GROUPS.ID"
+            "    LEFT OUTER JOIN GROUPS"
+            "    ON MATERNAL.GROUP_ID=GROUPS.ID"
             f" WHERE AC.ID={subject_id};"
         )
         self.assertEqual(expected_output, actual_output)
