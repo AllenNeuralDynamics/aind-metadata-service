672ee200d6a565cd1c3415668d5c8fab592237cb
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2022-12-08 15:59:03 -0800

    updates schema version in test client for procedures

--- tests/sharepoint/test_client.py
+++ tests/sharepoint/test_client.py
@@ -185,7 +185,7 @@ class Examples:
     )
     expected_procedures1 = Procedures.construct(
         describedBy=described_by,
-        schema_version="0.4.3",
+        schema_version="0.4.4",
         subject_id="650102",
         headframes=(
             [
