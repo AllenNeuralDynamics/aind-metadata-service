01288e780ec2d17aee42e14ded41c0746d964e68
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-02-08 16:38:44 -0800

    patch: statuscodes enum

--- src/aind_metadata_service/response_handler.py
+++ src/aind_metadata_service/response_handler.py
@@ -1,5 +1,6 @@
 """Module to handle responses"""
 from typing import List
+from enum import Enum
 
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
@@ -9,38 +10,51 @@ from pydantic import BaseModel, validate_model
 class Responses:
     """This class contains methods to map responses from server."""
 
-    @staticmethod
-    def internal_server_error_response() -> JSONResponse:
+    class StatusCodes(Enum):
+        """Enum class of status codes"""
+        connection_error = 503
+        internal_server_error = 500
+        multiple_responses = 300
+        valid_data = 200
+        invalid_data = 406
+        no_data = 404
+
+    def connection_error_response(self) -> JSONResponse:
+        """Map to a connection error"""
+        response = JSONResponse(
+            status_code=self.StatusCodes.connection_error.value,
+            content=({"message": "Error Connecting to Internal Server.", "data": None}),
+        )
+        return response
+
+    def internal_server_error_response(self) -> JSONResponse:
         """Map to an internal server error"""
         response = JSONResponse(
-            status_code=500,
+            status_code=self.StatusCodes.internal_server_error.value,
             content=({"message": "Internal Server Error.", "data": None}),
         )
         return response
 
-    @staticmethod
-    def no_data_found_response() -> JSONResponse:
+    def no_data_found_response(self) -> JSONResponse:
         """Map to a 404 error."""
         response = JSONResponse(
-            status_code=404,
+            status_code=self.StatusCodes.no_data.value,
             content=({"message": "No Data Found.", "data": None}),
         )
         return response
 
-    @staticmethod
-    def multiple_items_found_response(models: List[BaseModel]) -> JSONResponse:
-        """Map to a 418 error."""
+    def multiple_items_found_response(self, models: List[BaseModel]) -> JSONResponse:
+        """Map to a multiple choices error."""
         models_json = [jsonable_encoder(model) for model in models]
         response = JSONResponse(
-            status_code=418,
+            status_code=self.StatusCodes.multiple_responses.value,
             content=(
                 {"message": "Multiple Items Found.", "data": models_json}
             ),
         )
         return response
 
-    @staticmethod
-    def model_response(model: BaseModel) -> JSONResponse:
+    def model_response(self, model: BaseModel) -> JSONResponse:
         """
         Parse model to a response or return model if valid.
         Handles validation errors.
@@ -53,7 +67,7 @@ class Responses:
         model_json = jsonable_encoder(model)
         if validation_error:
             response = JSONResponse(
-                status_code=418,
+                status_code=self.StatusCodes.invalid_data.value,
                 content=(
                     {
                         "message": f"Validation Errors: {validation_error}",
@@ -63,7 +77,7 @@ class Responses:
             )
         else:
             response = JSONResponse(
-                status_code=200,
+                status_code=self.StatusCodes.valid_data.value,
                 content=(
                     {
                         "message": "Valid Model.",
