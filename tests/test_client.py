237897ac5e85e79977207fdb6138781cbafca845
Louise-X10 <77957732+Louise-X10@users.noreply.github.com>
2023-08-08 17:23:55 -0700

    tests: refactor to use map_to_json_response

--- tests/test_client.py
+++ tests/test_client.py
@@ -23,34 +23,22 @@ class TestAindMetadataServiceClient(unittest.TestCase):
         mock_subject_id = "00000"
         mock_response = requests.Response()
         mock_response.status_code = 200
-        mock_response._content = json.dumps(
-            {
-                "message": "Valid Model.",
-                "data": {
-                    "describedBy": "https://acme.com/subject.py",
-                    "schema_version": "0.2.2",
-                    "species": "Mus musculus",
-                    "subject_id": mock_subject_id,
-                    "sex": "Female",
-                    "date_of_birth": "2022-05-01",
-                    "genotype": "wt/wt",
-                    "mgi_allele_ids": None,
-                    "background_strain": None,
-                    "source": None,
-                    "rrid": None,
-                    "restrictions": None,
-                    "breeding_group": "breeding_group_id",
-                    "maternal_id": "00001",
-                    "maternal_genotype": "wt/wt",
-                    "paternal_id": "00002",
-                    "paternal_genotype": "wt/wt",
-                    "light_cycle": None,
-                    "home_cage_enrichment": None,
-                    "wellness_reports": None,
-                    "notes": None,
-                },
-            }
-        ).encode("utf-8")
+        model = Subject(
+            species=Species.MUS_MUSCULUS,
+            subject_id=mock_subject_id,
+            sex="Female",
+            date_of_birth="2022-05-01",
+            genotype="wt/wt",
+            breeding_group="breeding_group_id",
+            maternal_id="00001",
+            maternal_genotype="wt/wt",
+            paternal_id="00002",
+            paternal_genotype="wt/wt",
+        )
+        model_response = ModelResponse(
+            status_code=StatusCodes.DB_RESPONDED, aind_models=[model]
+        )
+        mock_response._content = model_response.map_to_json_response().body
 
         mock_get.return_value.__enter__.return_value = mock_response
 
@@ -124,40 +112,11 @@ class TestAindMetadataServiceClient(unittest.TestCase):
         mock_subject_id = "00000"
         mock_response = requests.Response()
         mock_response.status_code = 418
-        mock_response._content = json.dumps(
-            {
-                "message": "Validation Errors: 8 validation errors for "
-                "Procedures\nheadframes\n  'NoneType' object is not "
-                "iterable (type=type_error)\ncraniotomies\n  "
-                "'NoneType' object is not iterable "
-                "(type=type_error)\nmri_scans\n  'NoneType' object is "
-                "not iterable (type=type_error)\ninjections\n  "
-                "'NoneType' object is not iterable "
-                "(type=type_error)\nfiber_implants\n  "
-                "'NoneType' object is not iterable "
-                "(type=type_error)\ntraining_protocols\n  "
-                "'NoneType' object is not iterable "
-                "(type=type_error)\ntissue_preparations\n  "
-                "'NoneType' object is not iterable "
-                "(type=type_error)\nother_procedures\n  "
-                "'NoneType' object is not iterable (type=type_error)",
-                "data": {
-                    "describedBy": "https://acme.com/procedures.py",
-                    "schema_version": "0.4.4",
-                    "subject_id": mock_subject_id,
-                    "headframes": None,
-                    "craniotomies": None,
-                    "mri_scans": None,
-                    "injections": None,
-                    "fiber_implants": None,
-                    "water_restriction": None,
-                    "training_protocols": None,
-                    "tissue_preparations": None,
-                    "other_procedures": None,
-                    "notes": None,
-                },
-            }
-        ).encode("utf-8")
+        model = Procedures.construct(subject_id=mock_subject_id)
+        model_response = ModelResponse(
+            status_code=StatusCodes.DB_RESPONDED, aind_models=[model]
+        )
+        mock_response._content = model_response.map_to_json_response().body
 
         mock_get.return_value.__enter__.return_value = mock_response
 
