92e3c2bf8690ca554d9f4d78a274ff4275656cc4
Louise-X10 <77957732+Louise-X10@users.noreply.github.com>
2023-08-10 17:48:45 -0700

    feat: implement pickle option (#132)
    * feat: implement pickle option

--- src/aind_metadata_service/server.py
+++ src/aind_metadata_service/server.py
@@ -32,22 +32,24 @@ favicon_path = os.getenv("FAVICON_PATH")
 
 
 @app.get("/subject/{subject_id}")
-async def retrieve_subject(subject_id, pickle=False):
+async def retrieve_subject(subject_id, pickle: bool = False):
     """
     Retrieves subject from Labtracks server
-    TODO: pickle option will be handled in future versions
+    Returns pickled data if URL parameter pickle=True, else returns json
     """
     lb_client = LabTracksClient.from_settings(labtracks_settings)
-    response = lb_client.get_subject_info(subject_id)
-    json_response = response.map_to_json_response()
-    return json_response
+    model_response = lb_client.get_subject_info(subject_id)
+    if pickle:
+        return model_response.map_to_pickled_response()
+    else:
+        return model_response.map_to_json_response()
 
 
 @app.get("/procedures/{subject_id}")
-async def retrieve_procedures(subject_id, pickle=False):
+async def retrieve_procedures(subject_id, pickle: bool = False):
     """
-    Retrieves procedure info from SharePoint
-    TODO: pickle option will be handled in future versions
+    Retrieves procedure info from SharePoint and Labtracks servers
+    Returns pickled data if URL parameter pickle=True, else returns json
     """
     sharepoint_client = SharePointClient.from_settings(sharepoint_settings)
     lb_client = LabTracksClient.from_settings(labtracks_settings)
@@ -61,8 +63,10 @@ async def retrieve_procedures(subject_id, pickle=False):
     merged_response = sharepoint_client.merge_responses(
         [lb_response, sp2019_response, sp2023_response]
     )
-    json_response = merged_response.map_to_json_response()
-    return json_response
+    if pickle:
+        return merged_response.map_to_pickled_response()
+    else:
+        return merged_response.map_to_json_response()
 
 
 @app.get(
