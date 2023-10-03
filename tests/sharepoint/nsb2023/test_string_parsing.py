0a6541f33b9d3c94a12dd43b122d8254b3ffbf29
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-10-02 17:07:19 -0700

    Feat 123 update nsb2023 (#141)
    * remove str parsers
    
    * new models and mapping (in progress)
    
    * finished updating NSBList
    
    * maps burr 5 and 6
    
    * update examples to match new models
    
    * mapping updated
    
    * coverage + linters
    
    * interrogate
    
    * eof newlines
    
    * fix mappings
    
    * updates aind-data-schema
    
    * reformat resources
    
    * adds missing return Nones
    
    * volume list
    
    * feat: adds check on None input
    
    * volume list mapping
    
    * raw response str -> numeric
    
    ---------
    
    Co-authored-by: jtyoung84 <104453205+jtyoung84@users.noreply.github.com>

--- tests/sharepoint/nsb2023/test_string_parsing.py
+++ tests/sharepoint/nsb2023/test_string_parsing.py
@@ -63,41 +63,6 @@ class TestNSB2023StringParsers(TestCase):
                 self.assertEqual(expected, actual)
         self.assertIsNone(parser(None))
 
-    def test_ap_parser(self):
-        """Checks that ap fields are parsed correctly"""
-        ap_keys = [
-            "AP2ndInj",
-            "Burr3_x0020_A_x002f_P",
-            "Burr4_x0020_A_x002f_P",
-            "Virus_x0020_A_x002f_P",
-        ]
-        self._test_parser(ap_keys, self.blank_model._parse_ap_str)
-
-    def test_dv_parser(self):
-        """Checks that dv fields are parsed correctly"""
-        dv_keys = [
-            "DV2ndInj",
-            "FiberImplant1DV",
-            "FiberImplant2DV",
-            "Virus_x0020_D_x002f_V",
-            "Burr3_x0020_D_x002f_V",
-            "Burr4_x0020_D_x002f_V",
-            "Fiber_x0020_Implant3_x0020_D_x00",
-            "Fiber_x0020_Implant4_x0020_D_x00",
-        ]
-        self._test_parser(dv_keys, self.blank_model._parse_dv_str)
-
-    def test_ml_parser(self):
-        """Checks that ml fields are parsed correctly"""
-
-        ml_keys = [
-            "ML2ndInj",
-            "Burr3_x0020_M_x002f_L",
-            "Burr4_x0020_M_x002f_L",
-            "Virus_x0020_M_x002f_L",
-        ]
-        self._test_parser(ml_keys, self.blank_model._parse_ml_str)
-
     def test_alt_time_parser(self):
         """Checks that alternating time fields are parsed correctly"""
 
@@ -108,17 +73,6 @@ class TestNSB2023StringParsers(TestCase):
             "Inj4AlternatingTime",
         ]
         self._test_parser(at_keys, self.blank_model._parse_alt_time_str)
-
-    def test_angle_parser(self):
-        """Checks that angle fields are parsed correctly"""
-
-        an_keys = [
-            "Inj1Angle_v2",
-            "Inj2Angle_v2",
-            "Burr_x0020_3_x0020_Angle",
-            "Burr_x0020_4_x0020_Angle",
-        ]
-        self._test_parser(an_keys, self.blank_model._parse_angle_str)
 
     def test_current_parser(self):
         """Checks that current fields are parsed correctly"""
@@ -136,17 +90,6 @@ class TestNSB2023StringParsers(TestCase):
             "Inj4IontoTime",
         ]
         self._test_parser(lt_keys, self.blank_model._parse_length_of_time_str)
-
-    def test_volume_parser(self):
-        """Checks that volume fields are parsed correctly"""
-
-        v_keys = [
-            "Inj3volperdepth",
-            "Inj4volperdepth",
-            "inj1volperdepth",
-            "inj2volperdepth",
-        ]
-        self._test_parser(v_keys, self.blank_model._parse_inj_vol_str)
 
     def test_age_at_injection_parser(self):
         """Checks that age at injection fields are parsed correctly"""
