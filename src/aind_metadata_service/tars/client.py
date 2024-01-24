7919bb6f49a2617683e22fe13d78648733cb9a25
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2024-01-23 17:49:37 -0800

    added check for if genome name alr exists

--- src/aind_metadata_service/tars/client.py
+++ src/aind_metadata_service/tars/client.py
@@ -114,7 +114,10 @@ class TarsClient:
                 viral_prep_aliases = trh.map_virus_aliases(
                     aliases=lot["viralPrep"]["virus"]["aliases"]
                 )
-                if viral_prep_aliases.plasmid_name and viral_prep_aliases.full_genome_name is None:
+                if (
+                    viral_prep_aliases.plasmid_name
+                    and viral_prep_aliases.full_genome_name is None
+                ):
                     # check molecular registry for full genome name
                     molecule_response = self._get_molecules_response(
                         viral_prep_aliases.plasmid_name
