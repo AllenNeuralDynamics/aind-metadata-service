d092a827d1ad497e22d05991396ce69b54aa76df
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2025-02-03 17:20:53 -0800

    tests: add units tests for endpoints

--- src/aind_metadata_service/server.py
+++ src/aind_metadata_service/server.py
@@ -14,14 +14,14 @@ from aind_metadata_service.routers.healthcheck.route import router as hc_router
 from aind_metadata_service.routers.injection_materials.route import (
     router as inj_router,
 )
+from aind_metadata_service.routers.instrument.route import router as ins_router
 from aind_metadata_service.routers.perfusions.route import router as pf_router
 from aind_metadata_service.routers.project_names.route import (
     router as pn_router,
 )
 from aind_metadata_service.routers.protocols.route import router as pt_router
-from aind_metadata_service.routers.subject.route import router as subj_router
 from aind_metadata_service.routers.rig.route import router as rig_router
-from aind_metadata_service.routers.instrument.route import router as ins_router
+from aind_metadata_service.routers.subject.route import router as subj_router
 
 log_level = os.getenv("LOG_LEVEL", "INFO")
 logging.basicConfig(level=log_level)
