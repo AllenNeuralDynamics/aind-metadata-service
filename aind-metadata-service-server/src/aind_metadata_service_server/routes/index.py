e5fbff5794815ccdfb0fe6d36ccafe73ab3f5190
Helen Lin <helen.lin@alleninstitute.org>
2025-07-18 12:03:57 -0700

    feat: jinja template for subject and procedures search bars

--- aind-metadata-service-server/src/aind_metadata_service_server/routes/index.py
+++ aind-metadata-service-server/src/aind_metadata_service_server/routes/index.py
@@ -1,8 +1,18 @@
 """Module for the main index route"""
 
+from pathlib import Path
+
 from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+
+from aind_metadata_service_server import __version__ as service_version
 
 router = APIRouter()
+
+template_directory = str(
+    (Path(__file__).parent.parent / "templates").resolve()
+)
+templates = Jinja2Templates(directory=template_directory)
 
 
 @router.get("/")
@@ -10,4 +20,35 @@ async def index(request: Request):
     """
     Returns the index page with search UIs for enabled endpoints.
     """
-    # TODO: return Jinja2 template response
+    return templates.TemplateResponse(
+        name="index.html",
+        context=(
+            {
+                "request": request,
+                "service_name": "AIND Metadata Service",
+                "service_description": (
+                    "REST service to retrieve metadata from AIND databases."
+                ),
+                "service_version": service_version,
+                "endpoints": [
+                    {
+                        "endpoint": "subject",
+                        "description": (
+                            "Retrieve subject metadata from Labtracks server"
+                        ),
+                        "parameter": "subject_id",
+                        "parameter_label": "Subject ID",
+                    },
+                    {
+                        "endpoint": "procedures",
+                        "description": (
+                            "Retrieve procedures metadata from Labtracks and "
+                            "SharePoint servers"
+                        ),
+                        "parameter": "subject_id",
+                        "parameter_label": "Subject ID",
+                    },
+                ],
+            }
+        ),
+    )
