856ca1c16649e1e375d3f374fa34f55cf7954140
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2025-09-02 15:45:27 -0700

    feat: updates data-schema imports

--- aind-metadata-service-server/src/aind_metadata_service_server/models.py
+++ aind-metadata-service-server/src/aind_metadata_service_server/models.py
@@ -1,6 +1,6 @@
 """Models and schema definitions for backend data structures"""
 
-from typing import Literal
+from typing import Literal, Optional
 
 from pydantic import BaseModel
 
@@ -12,3 +12,13 @@ class HealthCheck(BaseModel):
 
     status: Literal["OK"] = "OK"
     service_version: str = __version__
+
+class IntendedMeasurementInformation(BaseModel):
+    """Intended Measurement information that will be returned to the user that
+    requests information from the NSB2023 Sharepoint."""
+
+    fiber_name: Optional[str] = None
+    intended_measurement_R: Optional[str] = None
+    intended_measurement_G: Optional[str] = None
+    intended_measurement_B: Optional[str] = None
+    intended_measurement_Iso: Optional[str] = None
