d8594f35ea438a3586b9b043a98ead25087ef95a
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2025-09-09 13:02:49 -0700

    feat: v2 smartsheet responses in procedures (#514)

--- aind-metadata-service-server/src/aind_metadata_service_server/models.py
+++ aind-metadata-service-server/src/aind_metadata_service_server/models.py
@@ -1,8 +1,8 @@
 """Models and schema definitions for backend data structures"""
 
-from typing import Literal
+from typing import Literal, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
 from aind_metadata_service_server import __version__
 
@@ -12,3 +12,25 @@ class HealthCheck(BaseModel):
 
     status: Literal["OK"] = "OK"
     service_version: str = __version__
+
+
+class ProtocolInformation(BaseModel):
+    """Protocol information that will be returned to the user that requests
+    information from the Protocols SmartSheet"""
+
+    protocol_type: str = Field(..., description="Protocol Type")
+    procedure_name: str = Field(..., description="Procedure name")
+    protocol_name: str = Field(..., description="Protocol name")
+    doi: str = Field(..., description="DOI")
+    version: str = Field(..., description="Version")
+    protocol_collection: Optional[str] = Field(
+        None, description="Protocol Collection"
+    )
+
+    @field_validator("version", "protocol_collection", mode="before")
+    def transform_version_to_str(cls, value) -> Optional[str]:
+        """Converts floats to strings"""
+        if value is None:
+            return None
+        else:
+            return str(value)
