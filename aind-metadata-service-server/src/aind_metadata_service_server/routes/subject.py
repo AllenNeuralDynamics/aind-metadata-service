a9aefe2fd6a82410a22b5048800c6244eb11d38f
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2025-11-10 18:54:25 -0500

    Turns MGI back on in api/v2/subject endpoint (#586)
    * turn mgi back on
    
    * fix test

--- aind-metadata-service-server/src/aind_metadata_service_server/routes/subject.py
+++ aind-metadata-service-server/src/aind_metadata_service_server/routes/subject.py
@@ -49,10 +49,10 @@ async def get_subject(
             logging.warning(
                 f"Skipping allele {allele_name} search for {subject_id}"
             )
-            # api_response = await mgi_api_instance.get_allele_info(
-            #     allele_name=allele_name, _request_timeout=10
-            # )
-            # mgi_info.extend(api_response)
+            api_response = await mgi_api_instance.get_allele_info(
+                allele_name=allele_name, _request_timeout=10
+            )
+            mgi_info.extend(api_response)
         mapper.mgi_info = mgi_info
 
     subjects = [mapper.map_to_aind_subject() for mapper in mappers]
