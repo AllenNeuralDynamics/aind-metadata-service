3b2a9efe86355d66af0a662f994e145406c23674
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2026-02-17 14:39:39 -0800

    Fix 638 filter dataverse table (#641)
    * fix: filters metadata fields from dataverse table
    
    * test: adds test for filtering metadata-keys
    
    * Keep lookup display values
    
    * mapper method should not be _
    
    * suggested fixes

---
+++ aind-metadata-service-server/src/aind_metadata_service_server/mappers/dataverse.py
@@ -0,0 +1,33 @@
+"""Module to handle dataverse data mapping and filtering"""
+
+from typing import Dict
+
+
+def filter_dataverse_metadata(data: Dict) -> Dict:
+    """
+    Filter out Dataverse metadata fields from the response.
+    Parameters
+    ----------
+    data : Dict
+        The dataverse response data
+
+    Returns
+    -------
+    Dict
+        Filtered data with metadata fields removed
+    """
+    if isinstance(data, dict):
+        return {
+            key: filter_dataverse_metadata(value)
+            for key, value in data.items()
+            if (
+                key.endswith("@OData.Community.Display.V1.FormattedValue")
+                or not (
+                    key.startswith("@") or "@" in key or key.startswith("_")
+                )
+            )
+        }
+    elif isinstance(data, list):
+        return [filter_dataverse_metadata(item) for item in data]
+    else:
+        return data
--- aind-metadata-service-server/src/aind_metadata_service_server/routes/dataverse.py
+++ aind-metadata-service-server/src/aind_metadata_service_server/routes/dataverse.py
@@ -3,6 +3,9 @@
 from aind_dataverse_service_async_client.exceptions import ApiException
 from fastapi import APIRouter, Depends, HTTPException, Path
 
+from aind_metadata_service_server.mappers.dataverse import (
+    filter_dataverse_metadata,
+)
 from aind_metadata_service_server.sessions import get_dataverse_api_instance
 
 router = APIRouter()
@@ -47,11 +50,12 @@ async def get_dataverse_table(
         dataverse_response = await dataverse_api_instance.get_table(
             entity_set_table_name, _request_timeout=10
         )
+        if not dataverse_response:
+            raise HTTPException(status_code=404, detail="Not found")
+        return filter_dataverse_metadata(dataverse_response)
+
     except ApiException as e:
         raise HTTPException(
             status_code=e.status,
             detail=f"Error fetching {entity_set_table_name}: {e.reason}",
         )
-    if not dataverse_response:
-        raise HTTPException(status_code=404, detail="Not found")
-    return dataverse_response
