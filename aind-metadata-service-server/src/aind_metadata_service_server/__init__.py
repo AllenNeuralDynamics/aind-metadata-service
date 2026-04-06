985d2cf885a9786e3cea4d45486ca67a48b6d396
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2026-04-06 16:25:19 -0700

    release v2.7.0

--- aind-metadata-service-server/src/aind_metadata_service_server/__init__.py
+++ aind-metadata-service-server/src/aind_metadata_service_server/__init__.py
@@ -8,7 +8,7 @@ from logging import LogRecord
 import yaml
 from pythonjsonlogger import json as log_json
 
-__version__ = "2.6.6"
+__version__ = "2.7.0"
 
 
 # We want to standardize the timestamp format to UTC and ISO-8601, which
