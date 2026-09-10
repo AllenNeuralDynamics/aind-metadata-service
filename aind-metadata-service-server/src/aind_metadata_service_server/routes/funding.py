fa8ec35e0d234b7068c49c745b1d9f460f1abafa
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2026-09-10 09:21:41 -0700

    fix: adds nsb procedure to v1 handler (#751)

--- aind-metadata-service-server/src/aind_metadata_service_server/routes/funding.py
+++ aind-metadata-service-server/src/aind_metadata_service_server/routes/funding.py
@@ -21,9 +21,7 @@ async def get_funding(
             "default": {
                 "summary": "A sample project name",
                 "description": "Example project name for smartsheet",
-                "value": (
-                    "Thalamus - Project 1 Mesoscale thalamic circuits"
-                ),
+                "value": ("Thalamus - Project 1 Mesoscale thalamic circuits"),
             }
         },
     ),
